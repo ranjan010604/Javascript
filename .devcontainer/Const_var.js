673e5e302fe2d498b6797cad8040fc6ea788fb25
Ranjan Kumar <kumarranjan62004@gmail.com>
2025-01-04 04:44:08 +0000

    const var dec done

---
+++ .devcontainer/Const_var.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "kumarranjan62004@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Jaipur"
+let accountStats ;
+
+// accountId = 2  not allowed
+accountEmail = "ranjankrtm@gmail.com"
+accountPassword = "98498"
+accountCity = "Bodhgaya"
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountStats]);
