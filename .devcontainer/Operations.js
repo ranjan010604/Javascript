fd572f8cf8b4d7103f1a1ec14b60b0ff1150aece
Ranjan Kumar <kumarranjan62004@gmail.com>
2025-01-06 14:25:55 +0000

    operation commit done

---
+++ .devcontainer/Operations.js
@@ -0,0 +1,22 @@
+/****************************** Opertions *****************************/
+// Variable assignment
+let value = 3;
+let negValue = -value; 
+console.log(negValue); 
+
+// Arithmetic operations
+console.log(2 + 2); 
+console.log(2 - 2); 
+console.log(2 * 2);
+console.log(2 / 3); 
+console.log(2 % 3); 
+
+// String concatenation
+let str1 = "hello";
+let str2 = "hitesh";
+let str3 = str1 + str2; 
+console.log(str3); 
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
