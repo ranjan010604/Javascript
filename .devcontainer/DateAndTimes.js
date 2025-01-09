15e57e94a60181e4390be23a62a9a2dbbe7ce898
Ranjan Kumar <kumarranjan62004@gmail.com>
2025-01-09 14:24:18 +0000

    date and time commit

--- .devcontainer/DateAndTimes.js
+++ .devcontainer/DateAndTimes.js
@@ -2,3 +2,14 @@ let myDate = new Date()
 console.log(myDate);
 console.log(myDate.toLocaleDateString()); 
 console.log(myDate.toLocaleString()); 
+console.log(typeof myDate);
+
+let myNewDate = new Date(2025, 0, 9)
+console.log(myNewDate.toDateString());
+let myCreatedDate = new Date(2025, 0, 9, 5, 3)
+console.log(myCreatedDate.toDateString());
+
+//mm dd yyyy
+let myOfcDate = new Date("2025-01-09")
+console.log(myOfcDate.toLocaleString());
+
